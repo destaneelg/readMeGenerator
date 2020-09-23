7526f4b9defbf293d0dd7dd88786142bb7026da9
Destanee Glenn <destanee.glenn@gmail.com>
2020-09-22 20:05:14 -0500

    adding first prompt and start to looping thorugh questions array

--- index.js
+++ index.js
@@ -3,9 +3,7 @@ var fs = require("fs");
 
 
 // array of questions for user
-const questions = [
-
-];
+const questions = ["What is the title of your project?", "How would you describe your project", "What technologies were used to make this project", "How is this intended to be used?", "Who contributed to this project?", "How was this product tested?",  "What license would you like to use?" ];
 
 // function to write README file
 function writeToFile(fileName, data) {
